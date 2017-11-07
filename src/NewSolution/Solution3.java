6fe1c88abb04b46fee1fec90f9b8173a157d3183
jiaqili <jiaqili@192.168.1.10>
2017-11-06 23:12:59 -0800

    Adding new package

---
+++ src/NewSolution/Solution3.java
@@ -0,0 +1,22 @@
+package NewSolution;
+
+import Util.ListNode;
+import Util.TreeNode;
+
+public class Solution3 {
+	public static void main(String[] args) {
+		String stringTest = "";
+		String[] stringArrayTest = {"", ""};
+		
+		ListNode node = new ListNode(0);
+		node.next = new ListNode(1);
+		node.next.next = new ListNode(2);
+		
+		TreeNode treeNode = new TreeNode(2);
+		treeNode.left = new TreeNode(1);
+		treeNode.right = new TreeNode(3);
+		
+		
+		System.out.println("");
+	}
+}
