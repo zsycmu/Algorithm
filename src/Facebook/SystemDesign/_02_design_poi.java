7531301fc2e8ac6983e1ec9b4b6e815d71b5ccda
Shengyi <Shengyi@shengyi-home>
2018-03-31 23:10:02 -0700

    Updating

--- src/Facebook/SystemDesign/_02_design_poi.java
+++ src/Facebook/SystemDesign/_02_design_poi.java
@@ -6,6 +6,10 @@ package Facebook.SystemDesign;
  * Point of Interest
  * 设计题先是给了经典的POI，我说我见过，把我的思路和网上流传的GeoHash说了一下。然后给我出了一个Designing Instagram。都很常规，设计为主。不过我没有探明考点是什么。
 
+
+第三轮design POI, geoHash解之。要求先估算需要多少machine，再说geohash。白板画需要几部分和之间关系，写backend POI meta data，算需要多少machine来存。最后有拍照。
+
+
  * @author Shengyi
  *
  */
